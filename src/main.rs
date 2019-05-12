25cea7c36783108973f6a0186128b10ae9181810
nczhu <nicolezhuis@gmail.com>
2019-05-12 13:34:21 +0200

    Added project starter code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/// Mandlebrot set: the set of complex numbers `c` for which
+/// z does NOT fly out to infinity. Where: 
+/// `z = z * z + c`
+/// 
+/// Mandlebrot plot: using c.real and c.imaginary as (x, y) 
+/// coordinates on a cartesian plane, we plot all c, where:
+/// Black point indicates `c` is in the Mandlebrot set
+/// White point indicates that `c` is NOT in the Mandelbrot set
+
+extern crate num;
+use num::Complex; //for complex number structs
 fn main() {
     println!("Hello, world!");
+}
+
+fn complex_square_add_loop(c: Complex<f64>){
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
 }
