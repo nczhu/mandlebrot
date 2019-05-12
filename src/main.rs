a4a5fbac5fca63bf2dcd7c0edf91e013059eb695
nczhu <nicolezhuis@gmail.com>
2019-05-12 13:56:44 +0200

    Add parse_complex ret Complex struct from input string, tests passing

--- src/main.rs
+++ src/main.rs
@@ -41,10 +41,23 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     }
 }
 
+// Parse a pair of floating-point numbers separated by a comma as a complex number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
 #[test]
 fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
     assert_eq!(parse_pair::<i32>(",20", ','), None);
     assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("0.5,1.5"), Some(Complex{ re: 0.5, im: 1.5 }));
 }
