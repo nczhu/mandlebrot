3b6f36c42be71c6f02d6cf696cfa0887f90aa765
nczhu <nicolezhuis@gmail.com>
2019-05-12 13:52:38 +0200

    Add: tests passing for parse_pair

--- src/main.rs
+++ src/main.rs
@@ -44,5 +44,7 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
 #[test]
 fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
-    
+    assert_eq!(parse_pair::<i32>(",20", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
